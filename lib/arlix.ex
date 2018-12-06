16a5cc77e43811068bafaa04188ba96543104669
angel <angel.imaz@hush.com>
2018-12-06 15:42:46 +0100

    Change String type to binary for data

--- lib/arlix.ex
+++ lib/arlix.ex
@@ -11,6 +11,6 @@ defmodule Arlix do
     require Record
     Record.defrecord(:tx,  id: <<>>, last_tx: <<>>, owner: <<>>, tags: [], target: <<>>, quantity: 0, data: <<>>, signature: <<>>, reward: 0)
 
-    @type tx :: record(:tx, id: String.t, last_tx: String.t, owner: String.t, tags: List.t, target: String.t, quantity: integer, data: String.t, signature: String.t, reward: integer)
+    @type tx :: record(:tx, id: String.t, last_tx: String.t, owner: String.t, tags: List.t, target: String.t, quantity: integer, data: binary(), signature: String.t, reward: integer)
   end
 end
